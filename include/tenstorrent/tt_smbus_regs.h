tenstorrent/tt-zephyr-platforms

DM2CM Debug Data

include/tenstorrent/tt_smbus_regs.h
@@ -42,6 +42,8 @@ enum CMFWSMBusReg {
 	CMFW_SMBUS_TEST_READ_BLOCK = 0xDC,
 	/* WO, 32 bits. Write to CMFW scratch register */
 	CMFW_SMBUS_TEST_WRITE_BLOCK = 0xDD,
+	/* WO, 40 bits. Write to specified Tensix-SM scratch RAM. */
+	CMFW_SMBUS_DEBUG_DATA = 0xDE,
 	CMFW_SMBUS_MSG_MAX,
 };
 

alewycky-tenstorrent/tt-system-firmware

include/tenstorrent/tt_smbus_regs.h
@@ -57,6 +57,8 @@ enum CMFWSMBusReg {
 	CMFW_SMBUS_TEST_WRITE_BLOCK = 0xDD,
 	/* WR, 32 bits I/O. Write to CMFW scratch register and read it back. */
 	CMFW_SMBUS_TEST_WRITE_BLOCK_READ_BLOCK = 0xDE,
+	/* WO, 40 bits. Write to specified Tensix-SM scratch RAM. */
+	CMFW_SMBUS_DEBUG_DATA = 0xE0,
 	CMFW_SMBUS_MSG_MAX,
 };
 